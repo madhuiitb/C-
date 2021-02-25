d725c013916cb563bca120b82ab2547e7c719bd4
madhuiitb <54939657+madhuiitb@users.noreply.github.com>
2021-02-25 17:23:12 +0530

    Update Two_Sum.cpp

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -8,16 +8,40 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        /*******************************************************************/
+        //________________________Brute Force_____________________________//
+        
+      //  vector<int> ret;
+      //  for(int i=0; i< nums.size()-1 ;i++){
+      //      for(int j=i+1; j< nums.size(); j++){
+      //          int tar = nums[i]+nums[j];
+      //          if(tar==target)
+      //          {
+      //              ret.push_back(i);
+      //              ret.push_back(j);
+      //          }
+      //      }
+      //  }
+      //  return ret;
+        
+        /*******************************************************************/
+        //________________________Linear Approach_________________________//
+        
+        unordered_map<int, int> set_values;
         vector<int> ret;
-        for(int i=0; i< nums.size()-1 ;i++){
-            for(int j=i+1; j< nums.size(); j++){
-                int tar = nums[i]+nums[j];
-                if(tar==target)
+        for(int i=0; i< nums.size() ;i++){
+                int tar = target - nums[i];
+                if(set_values.find(tar)!=set_values.end())
                 {
+                    ret.push_back(set_values[tar]);
                     ret.push_back(i);
-                    ret.push_back(j);
+                    return ret;
                 }
-            }
+                else
+                {
+                    set_values.insert(make_pair(nums[i],i));
+                }
         }
         return ret;
     }
